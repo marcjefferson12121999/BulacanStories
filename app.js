c26cc13c548bb4a6baaa53a6a8891dc85d7b71fa
Marc Jefferson <sumilangmj@gmail.com>
2020-11-15 02:11:13 +0800

    fifth commit attempt

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const dotenv = require ('dotenv')
 const morgan = require ('morgan')
-const exphbs = require ('exphbs')
+const exphbs = require ('express-handlebars')
 const connectDB = require("./config/db")
 
 // Load Config
